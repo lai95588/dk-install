d45bc82ef64e91a5aa1eca8d85ce09a60e770bde
lai95588 <47106943+lai95588@users.noreply.github.com>
2025-06-19 17:56:20 +0800

    Update nodered.sh

--- nodered.sh
+++ nodered.sh
@@ -2,11 +2,11 @@
 
 docker pull nodered/node-red;
 
-docker run -d --name=nodered --net=host --privileged --restart=always -it -p 1880:1880 -v /etc/localtime:/etc/localtime:ro -v /opt/hassio/nodered:/data nodered/node-red;
+docker run -d --name=nodered --net=host --privileged --restart=always -it -p 1880:1880 -v /etc/localtime:/etc/localtime:ro -v /home/hassio/nodered:/data nodered/node-red;
 
 sleep 2
 
-chmod -R 777 /opt/hassio/nodered
+chmod -R 777 /home/hassio/nodered
 
 docker exec nodered /bin/bash -c "npm install node-red-contrib-home-assistant-websocket;npm install node-red-contrib-modbus;npm install node-red-dashboard;";
 
