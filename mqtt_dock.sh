bf64ef75641f20fd7290341ad0159cff27790b65
lai95588 <47106943+lai95588@users.noreply.github.com>
2025-06-19 17:29:24 +0800

    Update mqtt_dock.sh

--- mqtt_dock.sh
+++ mqtt_dock.sh
@@ -2,24 +2,24 @@
 
 docker pull eclipse-mosquitto;
 
-mkdir -p /opt/hassio/mosquitto/config && mkdir -p /opt/hassio/mosquitto/data && mkdir -p /opt/hassio/mosquitto/log && touch /opt/hassio/mosquitto/config/mosquitto.conf;
+mkdir -p /home/hassio/mosquitto/config && mkdir -p /home/hassio/mosquitto/data && mkdir -p /home/hassio/mosquitto/log && touch /home/hassio/mosquitto/config/mosquitto.conf;
 
 sleep 2
 
-cat >> /opt/hassio/mosquitto/config/mosquitto.conf <<'EOF'
+cat >> /home/hassio/mosquitto/config/mosquitto.conf <<'EOF'
 persistence true
 persistence_location /mosquitto/data/
 log_dest file /mosquitto/log/mosquitto.log
 listener 1883
 EOF
 
-chmod -R 755 /opt/hassio/mosquitto && chmod -R 777 /opt/hassio/mosquitto/log;
+chmod -R 755 /home/hassio/mosquitto && chmod -R 777 /home/hassio/mosquitto/log;
 
-docker run -d --privileged -p 1883:1883 -p 9001:9001 --net=host --restart=always -it --name mosquitto -v /etc/localtime:/etc/localtime:ro -v /opt/hassio/mosquitto/config:/mosquitto/config/ -v /opt/hassio/mosquitto/data:/mosquitto/data -v /opt/hassio/mosquitto/log/:/mosquitto/log eclipse-mosquitto
+docker run -d --privileged -p 1883:1883 -p 9001:9001 --net=host --restart=always -it --name mosquitto -v /etc/localtime:/etc/localtime:ro -v /home/hassio/mosquitto/config:/mosquitto/config/ -v /home/hassio/mosquitto/data:/mosquitto/data -v /home/hassio/mosquitto/log/:/mosquitto/log eclipse-mosquitto
 
 sleep 5
 
-cat >> /opt/hassio/mosquitto/config/mosquitto.conf <<'EOF'
+cat >> /home/hassio/mosquitto/config/mosquitto.conf <<'EOF'
 allow_anonymous false
 password_file /mosquitto/config/pwfile.conf
 EOF
